400466d949d0596d2df79d36392d138e43f2bf10
key-forr <key.forr1@gmail.com>
2025-02-10 16:31:10 +0200

    added graphql and settings it, brought up redis in docker and configured configurations

--- prisma/generated/index.js
+++ prisma/generated/index.js
@@ -1,30 +1,28 @@
-
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true })
 
 const {
-  PrismaClientKnownRequestError,
-  PrismaClientUnknownRequestError,
-  PrismaClientRustPanicError,
-  PrismaClientInitializationError,
-  PrismaClientValidationError,
-  getPrismaClient,
-  sqltag,
-  empty,
-  join,
-  raw,
-  skip,
-  Decimal,
-  Debug,
-  objectEnumValues,
-  makeStrictEnum,
-  Extensions,
-  warnOnce,
-  defineDmmfProperty,
-  Public,
-  getRuntime,
-  createParam,
+	PrismaClientKnownRequestError,
+	PrismaClientUnknownRequestError,
+	PrismaClientRustPanicError,
+	PrismaClientInitializationError,
+	PrismaClientValidationError,
+	getPrismaClient,
+	sqltag,
+	empty,
+	join,
+	raw,
+	skip,
+	Decimal,
+	Debug,
+	objectEnumValues,
+	makeStrictEnum,
+	Extensions,
+	warnOnce,
+	defineDmmfProperty,
+	Public,
+	getRuntime,
+	createParam
 } = require('./runtime/library.js')
-
 
 const Prisma = {}
 
@@ -36,11 +34,11 @@ exports.$Enums = {}
  * Query Engine version: acc0b9dd43eb689cbd20c9470515d719db10d0b0
  */
 Prisma.prismaVersion = {
-  client: "6.3.1",
-  engine: "acc0b9dd43eb689cbd20c9470515d719db10d0b0"
+	client: '6.3.1',
+	engine: 'acc0b9dd43eb689cbd20c9470515d719db10d0b0'
 }
 
-Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
+Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError
 Prisma.PrismaClientUnknownRequestError = PrismaClientUnknownRequestError
 Prisma.PrismaClientRustPanicError = PrismaClientRustPanicError
 Prisma.PrismaClientInitializationError = PrismaClientInitializationError
@@ -57,8 +55,8 @@ Prisma.raw = raw
 Prisma.validator = Public.validator
 
 /**
-* Extensions
-*/
+ * Extensions
+ */
 Prisma.getExtensionContext = Extensions.getExtensionContext
 Prisma.defineExtension = Extensions.defineExtension
 
@@ -70,137 +68,139 @@ Prisma.JsonNull = objectEnumValues.instances.JsonNull
 Prisma.AnyNull = objectEnumValues.instances.AnyNull
 
 Prisma.NullTypes = {
-  DbNull: objectEnumValues.classes.DbNull,
-  JsonNull: objectEnumValues.classes.JsonNull,
-  AnyNull: objectEnumValues.classes.AnyNull
+	DbNull: objectEnumValues.classes.DbNull,
+	JsonNull: objectEnumValues.classes.JsonNull,
+	AnyNull: objectEnumValues.classes.AnyNull
 }
 
-
-
-
-  const path = require('path')
+const path = require('path')
 
 /**
  * Enums
  */
 exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
-  ReadUncommitted: 'ReadUncommitted',
-  ReadCommitted: 'ReadCommitted',
-  RepeatableRead: 'RepeatableRead',
-  Serializable: 'Serializable'
-});
+	ReadUncommitted: 'ReadUncommitted',
+	ReadCommitted: 'ReadCommitted',
+	RepeatableRead: 'RepeatableRead',
+	Serializable: 'Serializable'
+})
 
 exports.Prisma.UserScalarFieldEnum = {
-  id: 'id',
-  email: 'email',
-  password: 'password',
-  username: 'username',
-  displayName: 'displayName',
-  avatar: 'avatar',
-  bio: 'bio',
-  createdAt: 'createdAt',
-  updatedAt: 'updatedAt'
-};
+	id: 'id',
+	email: 'email',
+	password: 'password',
+	username: 'username',
+	displayName: 'displayName',
+	avatar: 'avatar',
+	bio: 'bio',
+	createdAt: 'createdAt',
+	updatedAt: 'updatedAt'
+}
 
 exports.Prisma.SortOrder = {
-  asc: 'asc',
-  desc: 'desc'
-};
+	asc: 'asc',
+	desc: 'desc'
+}
 
 exports.Prisma.QueryMode = {
-  default: 'default',
-  insensitive: 'insensitive'
-};
+	default: 'default',
+	insensitive: 'insensitive'
+}
 
 exports.Prisma.NullsOrder = {
-  first: 'first',
-  last: 'last'
-};
-
+	first: 'first',
+	last: 'last'
+}
 
 exports.Prisma.ModelName = {
-  User: 'User'
-};
+	User: 'User'
+}
 /**
  * Create the Client
  */
 const config = {
-  "generator": {
-    "name": "client",
-    "provider": {
-      "fromEnvVar": null,
-      "value": "prisma-client-js"
-    },
-    "output": {
-      "value": "D:\\Проекти\\keytostream\\keytostream-backend\\prisma\\generated",
-      "fromEnvVar": null
-    },
-    "config": {
-      "engineType": "library"
-    },
-    "binaryTargets": [
-      {
-        "fromEnvVar": null,
-        "value": "windows",
-        "native": true
-      }
-    ],
-    "previewFeatures": [],
-    "sourceFilePath": "D:\\Проекти\\keytostream\\keytostream-backend\\prisma\\schema.prisma",
-    "isCustomOutput": true
-  },
-  "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
-    "schemaEnvPath": "../../.env"
-  },
-  "relativePath": "..",
-  "clientVersion": "6.3.1",
-  "engineVersion": "acc0b9dd43eb689cbd20c9470515d719db10d0b0",
-  "datasourceNames": [
-    "db"
-  ],
-  "activeProvider": "postgresql",
-  "inlineDatasources": {
-    "db": {
-      "url": {
-        "fromEnvVar": "POSTGRES_URI",
-        "value": null
-      }
-    }
-  },
-  "inlineSchema": "generator client {\n  provider = \"prisma-client-js\"\n  output   = \"./generated\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n  url      = env(\"POSTGRES_URI\")\n}\n\nmodel User {\n  id String @id @default(uuid())\n\n  email    String @unique\n  password String\n\n  username    String @unique\n  displayName String @map(\"display_name\")\n\n  avatar String?\n  bio    String?\n\n  createdAt DateTime @default(now()) @map(\"created_at\")\n  updatedAt DateTime @updatedAt @map(\"updated_at\")\n\n  @@map(\"users\")\n}\n",
-  "inlineSchemaHash": "78620c42fbe636b5f53fbadf7026b5d3a0c3839f7ab8829f6f98d785c660e0b9",
-  "copyEngine": true
+	generator: {
+		name: 'client',
+		provider: {
+			fromEnvVar: null,
+			value: 'prisma-client-js'
+		},
+		output: {
+			value: 'D:\\Проекти\\keytostream\\keytostream-backend\\prisma\\generated',
+			fromEnvVar: null
+		},
+		config: {
+			engineType: 'library'
+		},
+		binaryTargets: [
+			{
+				fromEnvVar: null,
+				value: 'windows',
+				native: true
+			}
+		],
+		previewFeatures: [],
+		sourceFilePath:
+			'D:\\Проекти\\keytostream\\keytostream-backend\\prisma\\schema.prisma',
+		isCustomOutput: true
+	},
+	relativeEnvPaths: {
+		rootEnvPath: '../../.env',
+		schemaEnvPath: '../../.env'
+	},
+	relativePath: '..',
+	clientVersion: '6.3.1',
+	engineVersion: 'acc0b9dd43eb689cbd20c9470515d719db10d0b0',
+	datasourceNames: ['db'],
+	activeProvider: 'postgresql',
+	inlineDatasources: {
+		db: {
+			url: {
+				fromEnvVar: 'POSTGRES_URI',
+				value: null
+			}
+		}
+	},
+	inlineSchema:
+		'generator client {\n  provider = "prisma-client-js"\n  output   = "./generated"\n}\n\ndatasource db {\n  provider = "postgresql"\n  url      = env("POSTGRES_URI")\n}\n\nmodel User {\n  id String @id @default(uuid())\n\n  email    String @unique\n  password String\n\n  username    String @unique\n  displayName String @map("display_name")\n\n  avatar String?\n  bio    String?\n\n  createdAt DateTime @default(now()) @map("created_at")\n  updatedAt DateTime @updatedAt @map("updated_at")\n\n  @@map("users")\n}\n',
+	inlineSchemaHash:
+		'78620c42fbe636b5f53fbadf7026b5d3a0c3839f7ab8829f6f98d785c660e0b9',
+	copyEngine: true
 }
 
 const fs = require('fs')
 
 config.dirname = __dirname
 if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
-  const alternativePaths = [
-    "prisma/generated",
-    "generated",
-  ]
-  
-  const alternativePath = alternativePaths.find((altPath) => {
-    return fs.existsSync(path.join(process.cwd(), altPath, 'schema.prisma'))
-  }) ?? alternativePaths[0]
+	const alternativePaths = ['prisma/generated', 'generated']
 
-  config.dirname = path.join(process.cwd(), alternativePath)
-  config.isBundled = true
+	const alternativePath =
+		alternativePaths.find(altPath => {
+			return fs.existsSync(
+				path.join(process.cwd(), altPath, 'schema.prisma')
+			)
+		}) ?? alternativePaths[0]
+
+	config.dirname = path.join(process.cwd(), alternativePath)
+	config.isBundled = true
 }
 
-config.runtimeDataModel = JSON.parse("{\"models\":{\"User\":{\"dbName\":\"users\",\"schema\":null,\"fields\":[{\"name\":\"id\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":true,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"String\",\"nativeType\":null,\"default\":{\"name\":\"uuid\",\"args\":[4]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"email\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":true,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"password\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"username\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":true,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"displayName\",\"dbName\":\"display_name\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"avatar\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"bio\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":false,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"createdAt\",\"dbName\":\"created_at\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"DateTime\",\"nativeType\":null,\"default\":{\"name\":\"now\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"updatedAt\",\"dbName\":\"updated_at\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"DateTime\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":true}],\"primaryKey\":null,\"uniqueFields\":[],\"uniqueIndexes\":[],\"isGenerated\":false}},\"enums\":{},\"types\":{}}")
+config.runtimeDataModel = JSON.parse(
+	'{"models":{"User":{"dbName":"users","schema":null,"fields":[{"name":"id","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":true,"isReadOnly":false,"hasDefaultValue":true,"type":"String","nativeType":null,"default":{"name":"uuid","args":[4]},"isGenerated":false,"isUpdatedAt":false},{"name":"email","kind":"scalar","isList":false,"isRequired":true,"isUnique":true,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","nativeType":null,"isGenerated":false,"isUpdatedAt":false},{"name":"password","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","nativeType":null,"isGenerated":false,"isUpdatedAt":false},{"name":"username","kind":"scalar","isList":false,"isRequired":true,"isUnique":true,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","nativeType":null,"isGenerated":false,"isUpdatedAt":false},{"name":"displayName","dbName":"display_name","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","nativeType":null,"isGenerated":false,"isUpdatedAt":false},{"name":"avatar","kind":"scalar","isList":false,"isRequired":false,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","nativeType":null,"isGenerated":false,"isUpdatedAt":false},{"name":"bio","kind":"scalar","isList":false,"isRequired":false,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"String","nativeType":null,"isGenerated":false,"isUpdatedAt":false},{"name":"createdAt","dbName":"created_at","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":true,"type":"DateTime","nativeType":null,"default":{"name":"now","args":[]},"isGenerated":false,"isUpdatedAt":false},{"name":"updatedAt","dbName":"updated_at","kind":"scalar","isList":false,"isRequired":true,"isUnique":false,"isId":false,"isReadOnly":false,"hasDefaultValue":false,"type":"DateTime","nativeType":null,"isGenerated":false,"isUpdatedAt":true}],"primaryKey":null,"uniqueFields":[],"uniqueIndexes":[],"isGenerated":false}},"enums":{},"types":{}}'
+)
 defineDmmfProperty(exports.Prisma, config.runtimeDataModel)
 config.engineWasm = undefined
 config.compilerWasm = undefined
 
-
 const { warnEnvConflicts } = require('./runtime/library.js')
 
 warnEnvConflicts({
-    rootEnvPath: config.relativeEnvPaths.rootEnvPath && path.resolve(config.dirname, config.relativeEnvPaths.rootEnvPath),
-    schemaEnvPath: config.relativeEnvPaths.schemaEnvPath && path.resolve(config.dirname, config.relativeEnvPaths.schemaEnvPath)
+	rootEnvPath:
+		config.relativeEnvPaths.rootEnvPath &&
+		path.resolve(config.dirname, config.relativeEnvPaths.rootEnvPath),
+	schemaEnvPath:
+		config.relativeEnvPaths.schemaEnvPath &&
+		path.resolve(config.dirname, config.relativeEnvPaths.schemaEnvPath)
 })
 
 const PrismaClient = getPrismaClient(config)
@@ -208,8 +208,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "prisma/generated/query_engine-windows.dll.node")
+path.join(__dirname, 'query_engine-windows.dll.node')
+path.join(process.cwd(), 'prisma/generated/query_engine-windows.dll.node')
 // file annotations for bundling tools to include these files
-path.join(__dirname, "schema.prisma");
-path.join(process.cwd(), "prisma/generated/schema.prisma")
+path.join(__dirname, 'schema.prisma')
+path.join(process.cwd(), 'prisma/generated/schema.prisma')
---
+++ src/core/prisma/dto/update-prisma.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from '@nestjs/mapped-types'
+
+import { CreatePrismaDto } from './create-prisma.dto'
+
+export class UpdatePrismaDto extends PartialType(CreatePrismaDto) {}
