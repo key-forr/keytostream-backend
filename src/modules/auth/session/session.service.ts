320ed03243e293dca1fb40f4e8d497974288c321
key-forr <key.forr1@gmail.com>
2025-02-21 12:00:28 +0200

    Added two-factor authentication, password recovery with email notification

--- src/modules/auth/session/session.service.ts
+++ src/modules/auth/session/session.service.ts
@@ -9,6 +9,7 @@ import {
 import { ConfigService } from '@nestjs/config'
 import { verify } from 'argon2'
 import type { Request } from 'express'
+import { TOTP } from 'otpauth'
 
 import { PrismaService } from '@/src/core/prisma/prisma.service'
 import { RedisService } from '@/src/core/redis/redis.service'
@@ -75,7 +76,7 @@ export class SessionService {
 	}
 
 	public async login(req: Request, input: LoginInput, userAgent: string) {
-		const { login, password } = input
+		const { login, password, pin } = input
 
 		const user = await this.prismaService.user.findFirst({
 			where: {
@@ -102,6 +103,28 @@ export class SessionService {
 			throw new BadRequestException(
 				'Аккаунт не верифіковано. Будь ласка, перевірьте свою пошту для підтвердження аккаунта'
 			)
+		}
+
+		if (user.isTotpEnabled) {
+			if (!pin) {
+				return {
+					message: 'Необхідний код для завершення авторизації'
+				}
+			}
+
+			const totp = new TOTP({
+				issuer: 'keytostream',
+				label: `${user.email}`,
+				algorithm: 'SHA1',
+				digits: 6,
+				secret: user.totpSecret
+			})
+
+			const delta = totp.validate({ token: pin })
+
+			if (delta === null) {
+				throw new BadRequestException('Не вірний код')
+			}
 		}
 
 		const metadata = getSessionMetadata(req, userAgent)
