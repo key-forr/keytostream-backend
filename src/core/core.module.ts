37308fa9582faea65e754bf7de2cbe961af7ab4e
key-forr <key.forr1@gmail.com>
2025-03-06 19:38:39 +0200

    started stream

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -14,7 +14,9 @@ import { CronModule } from '../modules/cron/cron.module'
 import { LivekitModule } from '../modules/libs/livekit/livekit.module'
 import { MailModule } from '../modules/libs/mail/mail.module'
 import { StorageModule } from '../modules/libs/storage/storage.module'
+import { IngressModule } from '../modules/stream/ingress/ingress.module'
 import { StreamModule } from '../modules/stream/stream.module'
+import { WebhookModule } from '../modules/webhook/webhook.module'
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
 
 import { getGraphQLConfig } from './config/graphql.config'
@@ -52,7 +54,9 @@ import { RedisModule } from './redis/redis.module'
 		PasswordRecoveryModule,
 		TotpModule,
 		DeactivateModule,
-		StreamModule
+		StreamModule,
+		IngressModule,
+		WebhookModule
 	]
 })
 export class CoreModule {}
