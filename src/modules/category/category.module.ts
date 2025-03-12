2ea11a5e459751102078fc16e9f92a330503d6cf
key-forr <key.forr1@gmail.com>
2025-03-12 18:39:58 +0200

    seed file created, files added to s3 storage to corresponding seed file

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 
+import { CategoryResolver } from './category.resolver'
 import { CategoryService } from './category.service'
 
 @Module({
-	providers: [CategoryService]
+	providers: [CategoryResolver, CategoryService]
 })
 export class CategoryModule {}
